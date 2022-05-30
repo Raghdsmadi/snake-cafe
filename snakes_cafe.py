98e7926d6812511edd23c15841ec77c0e4dbfb0d
Raghdsmadi <raghadmohsmadi@hotmail.com>
2022-05-30 18:04:07 +0300

    updated

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -36,31 +36,34 @@ Unicorn Tears
 ***********************************
 """
 print(menu)
-items = ["Wings", "Cookies", "Spring Rolls","Salmon", "Steak","Meat Toranado", "A Litte Garden", "Ice Cream","Cake" , "Pie", "Coffee", "Tea", "Unicorn Tears" ]
-quantities = {item:0 for item in items}
+def input_user():
+    items = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Toranado", "A Litte Garden", "Ice Cream",
+             "Cake", "Pie", "Coffee", "Tea", "Unicorn Tears"]
+    quantities = {item: 0 for item in items}
 
-while True:
-    value = input("> ")
-    if value in items:
-        quantities[value] +=1
-        print(f"\n**{quantities[value]}order of {value} have been added to your meal**\n")
-    elif value == "quit":
-        break
-    elif value == "meal":
-        for i in [f"{key}: {val}" for key,val in quantities.items()if val!=0]:
-            print(i)
-    else:
-        print(f"Sorry ... \"{value}\"is not on the menu.")
-
-        while True:
-            answer= input("Would you like to order anything else ? (y/n) : ")
-            if (answer == "y") or (answer == "n"):
-                break
-            else:
-
-                print("Please select \"y\" or \"n\"\n")
-
-        if answer == "n":
+    while True:
+        value = input("> ")
+        if value in items:
+            quantities[value] += 1
+            print(f"\n**{quantities[value]}order of {value} have been added to your meal**\n")
+        elif value == "quit":
             break
+        elif value == "meal":
+            for i in [f"{key}: {val}" for key, val in quantities.items() if val != 0]:
+                print(i)
+        else:
+            print(f"Sorry ... \"{value}\"is not on the menu.")
 
-print("\nThanks for ordering :D \n")
+            while True:
+                answer = input("Would you like to order anything else ? (y/n) : ")
+                if (answer == "y") or (answer == "n"):
+                    break
+                else:
+
+                    print("Please select \"y\" or \"n\"\n")
+
+            if answer == "n":
+                break
+
+
+input_user()
